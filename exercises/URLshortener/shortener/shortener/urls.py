4cca081b5a93b0b8bb5c67f02e77835531dee161
Genadi Kupreishvili <genadikupreishvili@gmail.com>
2023-03-17 11:09:26 +0400

    add exercises

--- exercises/URLshortener/shortener/shortener/urls.py
+++ exercises/URLshortener/shortener/shortener/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('api/v1/', include('shortener.urls')),
 ]
